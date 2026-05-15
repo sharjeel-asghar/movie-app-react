8a8449d9a58338e4d63faee530f57aab7bf80d20
shary665 <retro786gaming665@gmail.com>
2026-05-15 20:31:05 +0500

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,15 @@ function Home (){
    {id:2,title:"Termintor", release_date:"2022"},
    {id:3,title:"The Matrix", release_date:"2019"},
   ]
+  const handleSearch  = () => {
+  
+  }
   return (
-    <div className='Home'>
+    <div className='home'>
+      <form onSubmit={handleSearch} className="search-name">
+        <input type="text" placeholder="Search for movies" className="search-input" />
+        <button type="submit" className="search-button">Search</button>
+      </form>
       <div className="movies-grid">
         {movies.map(movie =>( 
           <MovieCard movie={movie}  key={movie.id}/>
